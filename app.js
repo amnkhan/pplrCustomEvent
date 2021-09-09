6d7faf0ce3af8f50756750a039911b3e7ac2e93d
amnkhan <amnkhan.me@gmail.com>
2021-09-09 15:31:34 +0600

    pplrCustomEvent added

---
+++ app.js
@@ -0,0 +1,32 @@
+// Select add to cart button
+let atcBtn = document.querySelector(".add-to-cart");
+
+// Product Personalizer Custom Event Function
+function pplrCustomEvent() {
+  // Createing the custom event
+  const pplrCompleted = new 
+  CustomEvent("pplrCompleted", {
+    detail: {
+      pplrComepleted: true,
+    },
+    bubbles: true,
+    cancelable: true,
+    composed: false,
+  });
+  // Dispatch the event to window object, You can dispatch it anywhere you want
+  window.dispatchEvent(pplrCompleted);
+}
+
+// Call the custom event after user click on add to cart
+atcBtn.addEventListener("click", function(e){
+  e.preventDefault();
+  console.log("atc was clicked");
+  // custom event
+  pplrCustomEvent();
+});
+
+
+// Finally others can listen for the event like this
+window.addEventListener('pplrCompleted' , function(e) {
+  console.log(e.detail);
+})
